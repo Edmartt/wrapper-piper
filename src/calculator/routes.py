75e4ee5db358ff61e9a85285af58b4368cf63358
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-02 17:45:06 -0500

    chore: adds data access object for the distance calculator class view

--- src/calculator/routes.py
+++ src/calculator/routes.py
@@ -1,8 +1,15 @@
 from src.calculator.http_ids_handler import DistanceCalculator
+from src.calculator.data_layer.data_access_impl import PGDataAccess
+from src.calculator.models.location import Location
+from src.database.postgres import PostgresConnection
 from src.calculator.http_job_handler import Job
 from . import calculator_blueprint
 
-calc = DistanceCalculator.as_view('distance calculator')
+location = Location('', '', 0, 0)
+connector = PostgresConnection()
+data_access = PGDataAccess(connector, location)
+
+calc = DistanceCalculator.as_view('distance calculator', data_access)
 
 calculator_blueprint.add_url_rule('/api/v1/locations/ids', view_func=calc, methods=['POST'])
 
