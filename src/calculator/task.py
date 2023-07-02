df7abe10140290c3fae65295715aecf7ccdef1b4
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-02 05:50:55 -0500

    chore: adds logging for debugging

--- src/calculator/task.py
+++ src/calculator/task.py
@@ -1,7 +1,8 @@
+import logging
 from typing import List
 
 from rq import Queue
-from src.calculator.data.data_access_impl import PGDataAccess
+from src.calculator.data_layer.data_access_impl import PGDataAccess
 from src.calculator.models.location import Location
 from src.database.postgres import PostgresConnection
 from src.calculator.calc import calculate_distance
@@ -15,6 +16,11 @@ def extract_ids_from_inbound() -> list:
     conn = redis_conn.get_connection()
     id_list = conn.lrange('inbound', -2, -1)
     id_list = [id_.decode() for id_ in id_list]
+    logging.info(
+            str({
+                'ids from inbound': id_list
+                })
+            )
     return id_list
 
 def locations_dict_to_object(locations: List[dict]) -> List[Location]:
@@ -23,6 +29,9 @@ def locations_dict_to_object(locations: List[dict]) -> List[Location]:
     for location in locations:
         loc = Location('', location['name'], location['latitude'], location['longitude'])
         locations_list.append(loc)
+    logging.warning(str({
+        'locations instances': locations_list
+        }))
     return locations_list
 
 def send_to_outbound():
@@ -31,17 +40,33 @@ def send_to_outbound():
     location = Location('', '', 0, 0)
     data_access = PGDataAccess(connector, location)
     id_list = extract_ids_from_inbound()
+    logging.warning(str({
+        'list ready for outbound': id_list
+        }))
     
     locations = data_access.get_locations(id_list)
 
+    logging.warning(str({
+        'locations returned outbound': locations
+        }))
+
     locations_to_list = locations_dict_to_object(locations)
+
+    logging.warning(str({
+        'locations objects for outbound': id_list
+        }))
     distances = calculate_distance(locations_to_list)
+    print("distances: ", distances)
     conn.rpush('outbound', *distances)
 
     return distances
 
 def send_to_thread():
     job = task_queue.enqueue(send_to_outbound)
+
+    logging.warning(str({
+        'job send to thread': job
+        }))
 
     while not job.is_finished:
         job.refresh()
