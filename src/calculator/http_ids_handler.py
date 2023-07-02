e884f35f7e6a1360a2bc6a5cb0355e84da67ba1b
Edmartt <47486245+Edmartt@users.noreply.github.com>
2023-07-02 17:46:26 -0500

    fix: checks for non existant ids or when existing just one of the ids

--- src/calculator/http_ids_handler.py
+++ src/calculator/http_ids_handler.py
@@ -1,12 +1,13 @@
 from flask import current_app, jsonify, request
 from flask.views import MethodView
+from src.calculator.data_layer.data_access_interface import AccessDataInterface
 from src.calculator.task import send_to_thread
 from src.database.redis_mod import RedisConnection
 
 class DistanceCalculator(MethodView):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data_access: AccessDataInterface) -> None:
+        self.data_access = data_access
 
     def get(self):
         pass
@@ -18,6 +19,13 @@ class DistanceCalculator(MethodView):
 
         if len(ids) > 2 or len(ids) < 2:
             return jsonify({'response': 'please send two elements at once'}), 400
+        existant_ids = self.data_access.get_locations(ids)
+
+        if len(existant_ids) == 0:
+            return jsonify({'response': 'ids not found in database'}), 404
+
+        if len(existant_ids) < 2:
+            return jsonify({'response': 'some of the ids does not exists'}), 400
 
         r = RedisConnection()
 
